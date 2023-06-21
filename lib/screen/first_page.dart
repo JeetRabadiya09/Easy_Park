ae9e8f4c2381db193e4d9ac8e1c6876b3219fb67
Jeet Rabadiya <jeetrabadiya876@gmail.com>
2023-06-21 19:03:16 +0530

    welcome

--- lib/screen/first_page.dart
+++ lib/screen/first_page.dart
@@ -10,6 +10,60 @@ class FirstPage extends StatefulWidget {
 class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFF1D951A),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset("assets/images/welcome.png",
+                    width: 300, height: 250),
+                Positioned(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Column(
+                        children: [
+                          Text("EASY",
+                              style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: "CarterOne")),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Image.asset(
+                            "assets/images/CAR.png",
+                            height: 135,
+                            width: 78,
+                          ),
+                        ],
+                      ),
+                      const Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("PARK",
+                              style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: "CarterOne")),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
