ca250daca292a1fe9d60ee484e66c1f1b5fb4dab
Jeet Rabadiya <jeetrabadiya876@gmail.com>
2023-08-01 18:59:03 +0530

    changes

--- lib/screen/parking_ticket.dart
+++ lib/screen/parking_ticket.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_main_screen.dart';
+
 class ParkingTicket extends StatefulWidget {
   const ParkingTicket({Key? key}) : super(key: key);
 
@@ -10,6 +12,104 @@ class ParkingTicket extends StatefulWidget {
 class _ParkingTicketState extends State<ParkingTicket> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          children: [
+            AppBar(
+              leading: BackButton(
+                onPressed: () {},
+                color: const Color(0xFF000000),
+              ),
+              title: const Text(
+                "e- Ticket",
+                style: TextStyle(
+                    color: Color(0xFF000000),
+                    fontFamily: "sf-pro-display-cufonfonts",
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.more_vert,
+                      color: Color(0xFF000000),
+                    )),
+              ],
+            ),
+            Stack(
+              children: [
+                Image.asset("assets/images6/ticket.png"),
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: Image.asset(
+                        "assets/images6/Rectangle.png",
+                        width: 280,
+                        height: 150,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Image.asset(
+                      "assets/images6/ticket2.png",
+                      height: 126,
+                      width: 275,
+                    ),
+                    const SizedBox(
+                      height: 23,
+                    ),
+                    Image.asset(
+                      "assets/images6/ticket3.png",
+                      height: 194,
+                      width: 275,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomeMainScreen(),
+                  ),
+                );
+              },
+              child: Container(
+                height: 48,
+                width: double.infinity,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1D951A),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Text(
+                  "BACK TO HOME",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: "sf-pro-display-cufonfonts",
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
--- lib/screen/payment_success.dart
+++ lib/screen/payment_success.dart
@@ -1,3 +1,4 @@
+import 'package:easy_park/screen/parking_ticket.dart';
 import 'package:flutter/material.dart';
 
 import 'home_main_screen.dart';
@@ -25,7 +26,7 @@ class _PaymentSuccessState extends State<PaymentSuccess> {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => HomeMainScreen(),
+                            builder: (context) => const HomeMainScreen(),
                           ));
                     },
                     color: const Color(0xFF000000),
@@ -41,30 +42,322 @@ class _PaymentSuccessState extends State<PaymentSuccess> {
                           fontFamily: "sf-pro-display-cufonfonts")),
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 25,
               ),
-              Stack(
-                children: [
-                  Container(
-                    height: 450,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border:
-                            Border.all(width: 1.2, color: Color(0xFFB3B3B3))),
-                  ),
-                  Align(
-                    // alignment: Alignment.topCenter,
-                    heightFactor: 0.1,
-                    child: Image.asset(
-                      "assets/images6/payment.png",
-                      height: 82,
-                      width: 92,
+              Container(
+                height: 450,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border:
+                        Border.all(width: 1.2, color: const Color(0xFFB3B3B3))),
+                child: Stack(
+                  children: [
+                    Column(
+                      children: [
+                        Align(
+                          // alignment: Alignment.topCenter,
+                          heightFactor: 0.1,
+                          child: Image.asset(
+                            "assets/images6/payment.png",
+                            height: 82,
+                            width: 92,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        const Center(
+                          child: Text(
+                            "Payment Success!",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Color(0xFF1A1A1A),
+                                fontWeight: FontWeight.w600,
+                                fontFamily: "sf-pro-display-cufonfonts"),
+                          ),
+                        ),
+                        const Center(
+                          child: Text(
+                            "Your payment has been successfully done.",
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Color(0xFF4D4D4D),
+                                fontWeight: FontWeight.w500,
+                                fontFamily: "sf-pro-display-cufonfonts"),
+                          ),
+                        ),
+                        const Divider(
+                          indent: 15,
+                          endIndent: 15,
+                          height: 25,
+                          thickness: 1.5,
+                          color: Color(0xFF808080),
+                        ),
+                        const SizedBox(
+                          height: 05,
+                        ),
+                        const Center(
+                          child: Text(
+                            "Total Payment",
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xFF4D4D4D),
+                                fontWeight: FontWeight.w500,
+                                fontFamily: "sf-pro-display-cufonfonts"),
+                          ),
+                        ),
+                        const Center(
+                          child: Text(
+                            "INR 150",
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: Color(0xFF1A1A1A),
+                                fontWeight: FontWeight.w600,
+                                fontFamily: "Poppins"),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                Container(
+                                  height: 62,
+                                  width: 150,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(6),
+                                      border: Border.all(
+                                          color: const Color(0xFF808080),
+                                          width: 1)),
+                                  padding: const EdgeInsets.all(09),
+                                  child: const Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Ref Number",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: Color(0xFF808080),
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily:
+                                                "sf-pro-display-cufonfonts"),
+                                      ),
+                                      Text(
+                                        "000085752257",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: Color(0xFF1A1A1A),
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily:
+                                                "sf-pro-display-cufonfonts"),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  height: 62,
+                                  width: 150,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(6),
+                                      border: Border.all(
+                                          color: const Color(0xFF808080),
+                                          width: 1)),
+                                  padding: const EdgeInsets.all(09),
+                                  child: const Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Payment Method",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: Color(0xFF808080),
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily:
+                                                "sf-pro-display-cufonfonts"),
+                                      ),
+                                      Text(
+                                        "Bank Transfer",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: Color(0xFF1A1A1A),
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily:
+                                                "sf-pro-display-cufonfonts"),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Container(
+                                  height: 62,
+                                  width: 150,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(6),
+                                      border: Border.all(
+                                          color: const Color(0xFF808080),
+                                          width: 1)),
+                                  padding: const EdgeInsets.all(09),
+                                  child: const Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Payment Time",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: Color(0xFF808080),
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily:
+                                                "sf-pro-display-cufonfonts"),
+                                      ),
+                                      Text(
+                                        "15 Jun 2023, 13:22",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: Color(0xFF1A1A1A),
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily:
+                                                "sf-pro-display-cufonfonts"),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  height: 62,
+                                  width: 150,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(6),
+                                      border: Border.all(
+                                          color: const Color(0xFF808080),
+                                          width: 1)),
+                                  padding: const EdgeInsets.all(09),
+                                  child: const Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Sender Name",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: Color(0xFF808080),
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily:
+                                                "sf-pro-display-cufonfonts"),
+                                      ),
+                                      Text(
+                                        "Antonio Roberto",
+                                        style: TextStyle(
+                                            fontSize: 14,
+                                            color: Color(0xFF1A1A1A),
+                                            fontWeight: FontWeight.w500,
+                                            fontFamily:
+                                                "sf-pro-display-cufonfonts"),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 35,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "assets/images6/import.png",
+                              height: 24,
+                              width: 24,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            const Text(
+                              "Get PDF Receipt",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Color(0xFF4D4D4D),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "sf-pro-display-cufonfonts"),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  )
-                ],
+                  ],
+                ),
               ),
+              const SizedBox(
+                height: 35,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ParkingTicket(),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 48,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF1D951A),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Text(
+                    "VIEW TICKET",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: "sf-pro-display-cufonfonts",
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFFFFFFFF),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomeMainScreen(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  "Book Another",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: "sf-pro-display-cufonfonts",
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF1A1A1A),
+                  ),
+                ),
+              )
             ],
           ),
         ),
